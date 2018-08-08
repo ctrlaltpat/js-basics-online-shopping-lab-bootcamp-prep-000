320cf7d56ed45215f2969f49983d07d49a879c0a
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 19:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,15 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber){
+    var cart = getCart();
+    var total = total();
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  
+  
+  
 }
